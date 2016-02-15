3880540d1e5e4e6a9170a3c1309a32e5d32a0baa
zz <102223205@qq.com>
2016-02-15 23:33:22 +0800

    add pages

--- inviter.js
+++ inviter.js
@@ -6,19 +6,25 @@ var mySwiper = new Swiper ('.swiper-container', {
 		//alert(mySwiper.activeIndex)
 		if(mySwiper.activeIndex == 2){
 			run();
+		}else if(mySwiper.activeIndex == 3){
+			run3();
 		}
     }
 })
 function run(){
-	$('#run').html('0')
+	$('#run').html('0');
+	var time = 50;
 	function runadd(){
 		setTimeout(function(){
 			var year = $('#run').html();
-			$('#run').html(parseInt(year, 10) + 1)
+			$('#run').html(parseInt(year, 10) + 1);
 			if(year < 6){
 				runadd();
 			}
-		}, 50)
+		}, time)
 	}
 	runadd()
-}     
+}
+function run3(){
+
+}  
